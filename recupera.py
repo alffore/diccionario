10a40bd34d292451fec0a112d3861d9b8fc3fc44
A. Alfonso Flores R <alffore@gmail.com>
2019-04-07 23:53:47 -0500

    Documentación y preparación
    Se prepara el código para el trabajo básico de consulta a BD

--- recupera.py
+++ recupera.py
@@ -1,3 +1,12 @@
+'''
+Código inicial para recuperar información de PostgreSQL a traves de psycopg2 https://pypi.org/project/psycopg2/
+Adicionalmente se encuentra en GitHUB: https://github.com/alffore/diccionario
+
+AAFR alffore@gmail.com
+2019-04-08
+'''
+
+
 import psycopg2
 import psycopg2.extras
 
@@ -6,7 +15,7 @@ conn_dic = psycopg2.connect('dbname=DICSR host=127.0.0.1 port=5432 user=userreni
 
 def obtenDatos():
 
-    query="SELECT * FROM tablatfr Limit 10"
+    query="SELECT * FROM tablatfr Limit 1000"
     try:
         cursor = conn_dic.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cursor.execute(query)
