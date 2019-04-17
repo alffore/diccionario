9008224aa1ca2f5a8a181bb2b9834ec43dedfaaf
A. Alfonso Flores R <alffore@gmail.com>
2019-04-17 00:40:50 -0500

    Se incorpora la salida
    Este código incorpora la salida impresa tipo HTML

--- correlacion.py
+++ correlacion.py
@@ -8,30 +8,41 @@ AAFR, alffore@gmail.com
 
 import psycopg2
 import psycopg2.extras
-
 import math
+
+conn_dic = psycopg2.connect('dbname=DICSR host=127.0.0.1 port=5432 user=userrenic')
 
 
 def procesaentrada(ad):
     sid = 'i' + str(ad[0])
     alfa = math.pi / 4
-    r = round(255 * (1+math.sin(ad[1])) / 2)
-    g = round(255 * (1+math.sin(ad[1] + alfa)) / 2)
-    b = round(255 * (1+math.sin(ad[1] + 3 * alfa)) / 2)
+    r = round(255 * (1 + math.sin(ad[1])) / 2)
+    g = round(255 * (1 + math.sin(ad[1] + alfa)) / 2)
+    b = round(255 * (1 + math.sin(ad[1] + 3 * alfa)) / 2)
 
     color = 'rgb(' + str(r) + ',' + str(g) + ',' + str(b) + ')'
     return '<div id="' + sid + '" style="background-color:' + color + '">'
 
 
-conn_dic = psycopg2.connect('dbname=DICSR host=127.0.0.1 port=5432 user=userrenic')
+def imprimeHeader():
+    buff = '<html><head><style>  * {padding: 0px; margin: 0px;} .contendor {display: flex; width: 100%; height: ' \
+           '1200px;} div{width: 2px; height: 2px;}</style></head><body><div class="contenedor">'
+    return buff
 
-query = "SELECT tablatfr_id,peso FROM tablatfr WHERE trim(cadena) !~*'^$' LIMIT 10"
+
+def imprimeFooter():
+    return '</div></body></html>'
+
+
+print(imprimeHeader())
+
+query = "SELECT tablatfr_id,peso FROM tablatfr WHERE trim(cadena) !~*'^$' limit 500"
 try:
     cursor = conn_dic.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute(query)
 
     for res in cursor:
-        print(res)
+        # print(res)
         print(procesaentrada(res))
 
     cursor.close()
@@ -40,3 +51,5 @@ except (Exception, psycopg2.DatabaseError) as error:
     print(error)
 
 conn_dic.close()
+
+print(imprimeFooter())
