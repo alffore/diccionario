a0ae70a806b9fa999e2488e482df102b090365b5
A. Alfonso F. R <aalfonso@cultura.gob.mx>
2019-04-19 23:09:56 +0000

    Depurando

--- contador.py
+++ contador.py
@@ -1,4 +1,3 @@
-# coding=utf-8
 ''''
 Algoritmo que cuenta todas las palabras y hace una estadistica en la BD
 '''
@@ -9,6 +8,10 @@ from typing import Any
 
 conn_dic = psycopg2.connect('dbname=DICSR host=127.0.0.1 port=5432 user=userrenic')
 conn_sic = psycopg2.connect('dbname=nuevadbrenic host=127.0.0.1 port=5432 user=userrenic')
+# conn_sic.set_client_encoding('UTF8')
+# print(conn_sic.set_client_encoding('UTF8'))
+
+
 
 diccionario = {}
 
@@ -43,14 +46,21 @@ def procesaEntradaG(cad):
 
 def insertaDatos():
     for cad in diccionario:
-        peso = diccionario[cad]
-        query = 'INSERT INTO diccionario (cadena,peso) VALUES (\'' + cad + '\',' + str(peso) + ');'
-        print(query)
+        if( (cad is None) or (len(cad) == 0)):
+            continue
+        peso = diccionario[cad]        
+        cadi=u' '.join((cad,)).encode('utf-8').strip()
+        print(cadi)
+        print(cadi.decode('utf-8'))
+        #print(cadi.decode('utf-8','ignore'))
+        #print(cad.decode('utf-8','ignore').encode('utf-8'))
+        # query = 'INSERT INTO diccionario (cadena,peso) VALUES (\'' + cad + '\',' + str(peso) + ');'
+        # print(query)
     return
 
 
 '''Se procesa entradas de las busquedas'''
-query = "SELECT cadena,peso FROM tablatfr WHERE trim(cadena) !~*'^$' limit 1000"
+query = "SELECT cadena,peso FROM tablatfr WHERE trim(cadena) !~*'^$' limit 10"
 
 try:
     cursor = conn_dic.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -67,7 +77,7 @@ except (Exception, psycopg2.DatabaseError) as error:
 '''Se procesa entras de la BD del SIC-RENIC'''
 aux_campos = ['tabla', 'nombre', 'campo0', 'campo1', 'campo2', 'municipio', 'estado']
 apalabras = {}
-query = "SELECT tabla,nombre,campo0,campo1,campo2,municipio,estado FROM mvbusquedas limit 10"
+query = "SELECT tabla,nombre,campo0,campo1,campo2,municipio,estado FROM mvbusquedas"
 
 try:
     cursor_sic = conn_sic.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -76,10 +86,11 @@ try:
     for res in cursor_sic.fetchall():
         for c in aux_campos:
             if c != 'estado' or c != 'municipio':
-                apalabras = res[c].split(' ')
-                for palabra in apalabras:
-                    if len(palabra) > 0:
-                        procesaEntradaG(palabra)
+                if res[c] is not None:
+                    apalabras = res[c].split(' ')
+                    for palabra in apalabras:
+                        if len(palabra) > 0:
+                            procesaEntradaG(palabra)
             else:
                 procesaEntradaG(res[c])
 
