b1cfba1c599219fad0eddfb23b03476209735a20
A. Alfonso Flores R <alffore@gmail.com>
2019-04-19 19:32:12 -0500

    Se incluye procesamiento de campos de Busqueda
    Se procersan loscampos en los que se hace una busqueda

--- contador.py
+++ contador.py
@@ -6,6 +6,7 @@ import psycopg2
 import psycopg2.extras
 
 conn_dic = psycopg2.connect('dbname=DICSR host=127.0.0.1 port=5432 user=userrenic')
+conn_sic = psycopg2.connect('dbname=nuevadbrenic host=127.0.0.1 port=5432 user=userrenic')
 
 diccionario = {}
 
@@ -26,6 +27,16 @@ def procesaentrada(ares):
 
     return
 
+def procesaEntradaG(cad):
+    cad=cad.strip()
+
+    if cad in diccionario:
+        diccionario[cad] = diccionario[cad]+1
+    else
+        diccionario[cad] = 1
+
+    return
+
 
 def insertaDatos():
     for cad in diccionario:
@@ -34,7 +45,10 @@ def insertaDatos():
         print(query)
     return
 
+
+'''Método que procesa entradas de las busquedas'''
 query = "SELECT cadena,peso FROM tablatfr WHERE trim(cadena) !~*'^$' limit 1000"
+
 try:
     cursor = conn_dic.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute(query)
@@ -46,6 +60,30 @@ try:
 
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
+
+
+'''Método que procesa entras de la BD del SIC-RENIC'''
+aux_campos=['tabla','nombre','campo0','campo1','campo2','municipio','estado']
+query = "SELECT tabla,nombre,campo0,campo1,campo2,municipio,estado FROM mvbusquedas limit 10"
+
+try:
+    cursor_sic = conn_sic.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor_sic.execute(query)
+
+    for res in  cursor_sic.fetchall():
+        for c in aux_campos:
+            if c!='estado' or c!='municipio':
+                apalabras = res[c].split(' ')
+                for palabra in apalabra:
+                    if len(palabra) >0:
+                        procesaEntradaG(palabra)
+            else
+                procesaEntradaG(res[c])
+
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+
+
 
 conn_dic.close()
 
